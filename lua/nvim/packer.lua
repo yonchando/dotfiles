3654ea24863ba4f0d2e072e783bcae966597d80e
Yon Chando Bronx <chando.yon@bronxtechnology.com>
2023-09-12 15:18:49 +0700

    Add auto bootstrap packer manager and sync package

--- lua/nvim/packer.lua
+++ lua/nvim/packer.lua
@@ -1,4 +1,22 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
@@ -68,5 +86,9 @@ return require('packer').startup(function(use)
           "olimorris/neotest-phpunit"
       }
   }
+
+  if packer_bootstrap then
+      require('packer').sync()
+  end
 
 end)
