2757e216f3639379beede892eb505256707eb44f
Yon Chando Bronx <chando.yon@bronxtechnology.com>
2023-11-23 12:16:10 +0700

    Changed

--- lua/nvim/plugins/nvim-cmp.lua
+++ lua/nvim/plugins/nvim-cmp.lua
@@ -11,6 +11,7 @@ return {
         {
             "L3MON4D3/LuaSnip",
             version = "v2.*",
+            build = "make install_jsregexp"
         },
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
