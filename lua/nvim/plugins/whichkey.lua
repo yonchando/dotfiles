ffdd11600f4b2d1cefbeaadf33b3c81809f77d88
Yon Chando <von.chando@gmail.com>
2024-09-29 14:27:34 +0700

    fixed which-key

--- lua/nvim/plugins/whichkey.lua
+++ lua/nvim/plugins/whichkey.lua
@@ -4,23 +4,9 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 500
         require("which-key").setup {
-            window = {
+            win = {
                 border = "single"
             },
-            operators = {
-                gc = "Comments"
-            },
-            motions = {
-                count = true,
-            },
-            popup_mappings = {
-                scroll_down = "<c-d>",
-                scroll_up = "<c-u>",
-            },
-            triggers_blacklist = {
-                i = { "<" },
-                n = { "d", "c" }
-            }
         }
     end
 }
