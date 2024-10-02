447e76bdd3425dae5299b59ba0da220693810f76
Yon Chando <von.chando@gmail.com>
2024-10-02 13:56:43 +0700

    fixed setup-env

--- setup-env.sh
+++ setup-env.sh
@@ -5,19 +5,25 @@ sudo apt update && sudo apt install -y curl git
 # Set up oh my zsh
 if [[ ! -x $(which zsh) ]]; then
     sudo apt install -y zsh-autosuggestions zsh-syntax-highlighting zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    # auto sugesstion
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    # cmd higlight syntax
-    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-    # cmd autocomplete
-    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-    # oh my theme powerlevel10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    # clone oh my zsh
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
     # cp zshrc config
     cp .zshrc ~/.zshrc
+
+    # change shell
+    chsh -s $(which zsh)
+
+    # auto sugesstion
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git {$ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # cmd higlight syntax
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+    # cmd autocomplete
+    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+    # oh my theme powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 
     source ~/.zshrc
 fi
