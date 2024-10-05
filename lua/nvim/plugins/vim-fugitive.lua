e7e2f89786667a9485d2ecd18d13a9ac0639f3df
Yon Chando <von.chando@gmail.com>
2024-10-05 21:26:06 +0700

    git keymap

--- lua/nvim/plugins/vim-fugitive.lua
+++ lua/nvim/plugins/vim-fugitive.lua
@@ -5,9 +5,6 @@ return {
         vim.keymap.set("n", "<leader>gc", function() vim.cmd('Git commit') end, { desc = "Git Commits" })
         vim.keymap.set("n", "<leader>gd", function() vim.cmd('Git diff') end, { desc = "Git diff" })
         vim.keymap.set("n", "<leader>gl", function() vim.cmd('Git log') end, { desc = "Git log" })
-        vim.keymap.set("n", "<leader>gaa", function() vim.cmd('Git add .') end, { desc = "Git add all" })
         vim.keymap.set("n", "<leader>gmtl", function() vim.cmd('Git mergetool') end, { desc = "Git mergetool" })
-        vim.keymap.set("n", "<leader>gp", function() vim.cmd('Git push') end, { desc = "Git push" })
-        vim.keymap.set("n", "<leader>gl", function() vim.cmd('Git pull') end, { desc = "Git pull" })
     end
 }
