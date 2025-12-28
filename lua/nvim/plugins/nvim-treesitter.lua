4cdd6224b11d656396307c0ec3f2a51a3f379b24
Yon Chando <von.chando@gmail.com>
2025-12-28 11:53:32 +0700

    Enabled treesitter_textobjects and config keymap

--- lua/nvim/plugins/nvim-treesitter.lua
+++ lua/nvim/plugins/nvim-treesitter.lua
@@ -1,7 +1,53 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-        -- "nvim-treesitter/nvim-treesitter-textobjects",
+        {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            branch = "main",
+            config = function()
+                local treesitter_textobjects = require("nvim-treesitter-textobjects")
+                local select = require("nvim-treesitter-textobjects.select")
+
+                treesitter_textobjects.setup({
+                    select = {
+                        lookahead = true,
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V',  -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    }
+                })
+
+                vim.keymap.set({ "x", "o" }, "aa", function()
+                    select.select_textobject("@parameter.outer", "textobjects")
+                end)
+                vim.keymap.set({ "x", "o" }, "ia", function()
+                    select.select_textobject("@parameter.inner", "textobjects")
+                end)
+                vim.keymap.set({ "x", "o" }, "af", function()
+                    select.select_textobject("@function.outer", "textobjects")
+                end)
+                vim.keymap.set({ "x", "o" }, "if", function()
+                    select.select_textobject("@function.inner", "textobjects")
+                end)
+                vim.keymap.set({ "x", "o" }, "ac", function()
+                    select.select_textobject("@class.outer", "textobjects")
+                end)
+                vim.keymap.set({ "x", "o" }, "ic", function()
+                    select.select_textobject("@class.inner", "textobjects")
+                end)
+                -- You can also use captures from other query groups like `locals.scm`
+                vim.keymap.set({ "x", "o" }, "as", function()
+                    select.select_textobject("@local.scope", "locals")
+                end)
+
+                vim.keymap.set("n", "<leader>a", function()
+                    require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
+                end)
+            end
+        },
     },
     lazy = false,
     build = ":TSUpdate",
